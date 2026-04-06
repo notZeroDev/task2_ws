d4a1629f1025cb65b493c1c3f5d271d172f5a07f
youssef <yousefkamel282@gmail.com>
2026-04-06 17:51:08 +0200

    edited the object detector to make it use a custom msg instead of string

--- src/surveillance_system/surveillance_system/nodes/object_detector_node.py
+++ src/surveillance_system/surveillance_system/nodes/object_detector_node.py
@@ -7,6 +7,7 @@ from std_msgs.msg import String
 from cv_bridge import CvBridge
 import cv2
 from ultralytics import YOLO
+from perception_interfaces.msg import BoundingBox, DetectionList
 
 class ObjectDetectionNode(Node):
     def __init__(self):
@@ -27,16 +28,25 @@ class ObjectDetectionNode(Node):
             10
         )
 
-        self.publisher_ = self.create_publisher(String, '/detected_objects', 10)
+        self.publisher_ = self.create_publisher(DetectionList, '/detected_objects', 10)
         self.bridge = CvBridge()
         self.get_logger().info("Object Detection Node started")
 
     def image_callback(self, msg):
+        # extract info from image
+        img_msg_header = msg.header
+        msg_timestamp = img_msg_header.stamp
+        msg_frame_id = img_msg_header.frame_id
+
+        # decoding msg payload pack to image
         frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
 
         results = self.model(frame, device='cpu', verbose=False)
+        self.get_logger().info(f"Detection is running of frame {msg_frame_id} | {frame.shape}")
 
-        detections = []
+        # detections = []
+        detection_list_msg = DetectionList()
+        detection_list_msg.header = img_msg_header
 
         for r in results:
             for box in r.boxes:
@@ -48,11 +58,25 @@ class ObjectDetectionNode(Node):
                 label = self.model.names[cls]
 
                 x1, y1, x2, y2 = map(int, box.xyxy[0])
-                detections.append(f"{label} {conf:.2f} [{x1},{y1},{x2},{y2}]")
+                # detections.append(f"{label} {conf:.2f} [{x1},{y1},{x2},{y2}]")
+                bbox = BoundingBox()
+                bbox.x1 = x1
+                bbox.y1 = y1
+                bbox.x2 = x2
+                bbox.y2 = y2
+                bbox.conf = conf
+                bbox.class_name = label
+                detection_list_msg.detections.append(bbox)
 
-        msg_out = String()
-        msg_out.data = "; ".join(detections)
-        self.publisher_.publish(msg_out)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.putText(frame, f'{label}', (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+        
+        cv2.imshow("2D Detections", frame)
+        cv2.waitKey(1)
+
+        # msg_out = String()
+        # msg_out.data = "; ".join(detections)
+        self.publisher_.publish(detection_list_msg)
 
 def main(args=None):
     rclpy.init(args=args)
