a849df5fd459c52f895abcd07faa041b5d1d6349
youssef <yousefkamel282@gmail.com>
2026-04-06 17:51:26 +0200

    edited depth to publish Image instead of string

--- src/surveillance_system/surveillance_system/nodes/depth_estimator_node.py
+++ src/surveillance_system/surveillance_system/nodes/depth_estimator_node.py
@@ -13,8 +13,12 @@ import numpy as np
 import torch
 import concurrent.futures
 
-# Add Depth-Anything-V2 to sys.path
-sys.path.append('/root/task2_ws/Depth-Anything-V2')
+# sys.path.append('/ros2/task2_ws/Depth-Anything-V2')
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# Go up 3 levels: nodes -> surveillance_system -> surveillance_system -> src -> TASK2_WS
+ws_root = os.path.join(script_dir, "../../../..") 
+sys.path.append(os.path.join(ws_root, 'Depth-Anything-V2'))
+
 from depth_anything_v2.dpt import DepthAnythingV2
 
 class DepthEstimatorNode(Node):
@@ -22,7 +26,7 @@ class DepthEstimatorNode(Node):
         super().__init__('depth_estimator_node')
 
         # Parameters
-        self.declare_parameter('depth_model_path', '/root/task2_ws/checkpoints/depth_anything_v2_vits.pth')
+        self.declare_parameter('depth_model_path', '/home/youssef/ros2/task2_ws/checkpoints/depth_anything_v2_vits.pth')
         self.declare_parameter('encoder', 'vits')
         self.declare_parameter('process_every_n', 3)
         self.declare_parameter('near_threshold', 0.65)
@@ -34,8 +38,8 @@ class DepthEstimatorNode(Node):
         self.near_threshold = self.get_parameter('near_threshold').value
         self.input_size = self.get_parameter('input_size').value
 
-        self.frame_count = 0
-        self.depth_frame_id = 0
+        # self.frame_count = 0
+        # self.depth_frame_id = 0
 
         # Load the model
         self.get_logger().info(f"Loading DepthAnythingV2 ({self.encoder})...")
@@ -59,7 +63,7 @@ class DepthEstimatorNode(Node):
             self.image_callback,
             2
         )
-        self.publisher_ = self.create_publisher(String, '/object_depth', 10)
+        self.publisher_ = self.create_publisher(Image, '/object_depth', 10)
         
         self.bridge = CvBridge()
         
@@ -68,44 +72,58 @@ class DepthEstimatorNode(Node):
         self.future = None
 
     def image_callback(self, msg):
-        self.frame_count += 1
+        # extract info from image
+        self.img_msg_header = msg.header
+        msg_timestamp =  self.img_msg_header.stamp
+        msg_frame_id =  self.img_msg_header.frame_id
         
-        if self.frame_count % self.process_every_n != 0:
-            return
+        # if self.frame_count % self.process_every_n != 0:
+        #     return
 
         # Ensure we don't queue multiple inferences if CPU is lagging
         if self.future is not None and not self.future.done():
             return
             
         frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-        self.depth_frame_id += 1
         
         # Submit to executor, passing current frame_id
-        self.future = self.thread_executor.submit(self.infer_depth, frame, self.depth_frame_id)
+        self.future = self.thread_executor.submit(self.infer_depth, frame, msg_frame_id)
         self.future.add_done_callback(self.publish_result)
+        
 
     def infer_depth(self, frame, frame_id):
-        # model.infer_image expects BGR input and input_size
-        depth = self.model.infer_image(frame, input_size=self.input_size)
+        # 1. Inference
+        depth_map = self.model.infer_image(frame, input_size=self.input_size)
         
-        # Depth is raw disparity, normalize to 0-1 for scene analyzer
-        depth_min, depth_max = depth.min(), depth.max()
+        # 2. Create the ROS message
+        depth_msg = self.bridge.cv2_to_imgmsg(depth_map.astype(np.float32), encoding='32FC1')
+        depth_msg.header = self.img_msg_header
+        
+        # Fix: Use depth_map here, not depth
+        self.get_logger().info(f"Depth estimation frame {frame_id} | depth_map shape {depth_map.shape}")
+        
+        # 3. Visualization Logic
+        depth_min, depth_max = depth_map.min(), depth_map.max()
         if depth_max > depth_min:
-            depth_norm = (depth - depth_min) / (depth_max - depth_min)
+            depth_vis = 255*(depth_map - depth_min) / (depth_max - depth_min)
         else:
-            depth_norm = depth
-            
-        # Compute overall stats
-        avg_depth = float(np.mean(depth_norm))
-        near_ratio = float(np.mean(depth_norm > self.near_threshold))
+            depth_vis = np.zeros_like(depth_vis, 0, 255)
+
+        depth_vis = np.clip(depth_vis, 0, 255).astype(np.uint8)
+        # depth_colored = cv2.applyColorMap(depth_vis, cv2.COLORMAP_JET)    
+
+        # depth_resized = cv2.resize(depth_colored, (0, 0), fx=0.5, fy=0.5)
+        # frame_resized = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+        # combined_view = np.hstack((frame_resized, depth_resized))
+
+        # cv2.imshow("Depth_Estimation", combined_view)
+        # cv2.waitKey(1)
         
-        return f"frame_id={frame_id}; avg_depth={avg_depth:.2f}; near_ratio={near_ratio:.2f}; min={depth_min:.2f}; max={depth_max:.2f}"
+        return depth_msg
 
     def publish_result(self, future):
         try:
-            result_str = future.result()
-            msg = String()
-            msg.data = result_str
+            msg = future.result()
             self.publisher_.publish(msg)
         except Exception as e:
             self.get_logger().error(f"Inference failed: {e}")
